89179f662a112a4de7d2f22dbb9a39d857e749ac
JaufreLallement <lallement.jaufre@gmail.com>
2019-12-10 16:15:52 +0100

    Edit: add some comments

--- consonant_vowel.py
+++ consonant_vowel.py
@@ -1,12 +1,13 @@
 #!/usr/local/bin/python3.8
 # -*-coding:Utf-8 -*
 
-chaine = input("Saisissez une chaîne de caractères: ")
+chaine = input("Saisissez une chaîne de caractères: ") # The user enters a string
 
+# For every char|letter in the string
 for lettre in chaine:
-    if lettre in "AEIOUYaeiouy":
+    if lettre in "AEIOUYaeiouy": # If the current char is a vowel
         print(f"{lettre}: voyelle")
-    elif lettre in "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz":
+    elif lettre in "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz": # If the current char is a consonant
         print(f"{lettre}: consonne")
     else:
         print(lettre)
--- multiplication_table.py
+++ multiplication_table.py
@@ -1,11 +1,13 @@
 #!/usr/local/bin/python3.8
 # -*-coding:Utf-8 -*
 
-in_table = input("Saisissez un nombre: ")
-n_table = int(in_table)
+in_table = input("Saisissez un nombre: ") # The user inputs a number
+n_table = int(in_table) # Conversion from string to integer
 i = 1
 
 print(f"%%%%%%%%%%%% Table de {n_table} %%%%%%%%%%%%")
+
+# Prints multiplication table of the entered number
 while i <= 10:
     print(f"{n_table} * {i} = {n_table * i}")
     i += 1
